66ebcecb395f4cc744f45c588d8edde6f06a6a6f
12999355@itisrossi.vi.it <12999355@itisrossi.vi.it>
2026-03-09 21:02:16 +0100

    Fine parte console

---
+++ Heap--Manuzzato/Heap- Manuzzato/Heap.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap__Manuzzato
+{
+    internal class Heap
+    {
+        public int Count { get; private set; }
+
+        private int[] _elements;
+
+        public Heap()
+        {
+            _elements = new int[100];
+            this[0] = -1;
+            Count = 0;
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                return _elements[i];
+            }
+            set
+            {
+                _elements[i] = value;
+            }
+        }
+
+        public void Insert(int element)
+        {
+            this[++Count] = element;
+            GoUp(Count);
+        }
+
+        public int Remove()
+        {
+            if (Count == 0)
+                throw new IndexOutOfRangeException("Heap vuoto");
+
+            int smallest = this[1];
+            this[1] = this[Count];
+            Count--;
+
+            GoDown(1);
+            return smallest;
+        }
+
+        public int Peek()
+        {
+            if (Count == 0)
+                throw new IndexOutOfRangeException("Heap vuoto");
+            return this[1];
+        }
+
+        public int GetParent(int i)
+        {
+            if (i == 1)
+                throw new IndexOutOfRangeException("Il nodo radice non ha un padre");
+            return this[i / 2];
+        }
+
+        private void GoUp(int currentindex)
+        {
+            if (currentindex == 1)
+                return;
+
+            int fatherIndex = currentindex / 2;
+
+            if (this[currentindex] < this[fatherIndex])
+            {
+                Swap(currentindex, fatherIndex);
+                GoUp(fatherIndex);
+            }
+        }
+
+        private void GoDown(int i)
+        {
+            int leftSon = i * 2;
+            int rightSon = i * 2 + 1;
+
+            if (leftSon > Count)
+                return;
+
+            int smallest = leftSon;
+            if (rightSon <= Count && this[rightSon] < this[leftSon])
+                smallest = rightSon;
+
+            if (this[i] <= this[smallest])
+                return;
+
+            Swap(i, smallest);
+            GoDown(smallest);
+        }
+
+        private void Swap(int i, int j) 
+        {
+            int temp = this[i];
+            this[i] = this[j];
+            this[j] = temp;
+        }
+    }
+}
---
+++ Heap--Manuzzato/Heap- Manuzzato/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Heap__Manuzzato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            Heap heap = new Heap();
+
+            while (true) 
+            {
+                do
+                {
+
+
+                    Console.WriteLine("Insert a number in the heap (or 'exit' to quit):");
+
+                    
+                } while (!int.TryParse(Console.ReadLine(), out n) || n<0);
+
+              
+                    heap.Insert(n);
+
+                PrintHeap(heap);
+            }
+
+           
+        }
+
+        static void PrintHeap(Heap heap)
+        {
+            for (int i = 1; i <= heap.Count; i++)
+            {
+                Console.Write( "[" + heap[i] + " ]");
+            }
+            Console.WriteLine();
+        }
+    }
+}
